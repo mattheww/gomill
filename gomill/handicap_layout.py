c844b162d29542769544dbd3def5b7f508e28b17
Matthew Woodcraft <matthew@woodcraft.me.uk>
2009-08-21 23:20:43 +0100

    Cope with handicap patterns on different board sizes as in GTP spec

--- gomill/handicap_layout.py
+++ gomill/handicap_layout.py
@@ -4,7 +4,7 @@ This follows the rules from the GTP spec.
 
 """
 
-handicap_9x9 = [
+handicap_pattern = [
     ['C3', 'G7'],
     ['C3', 'G7', 'C7'],
     ['C3', 'G7', 'C7', 'G3'],
@@ -15,8 +15,6 @@ handicap_9x9 = [
     ['C3', 'G7', 'C7', 'G3', 'C5', 'G5', 'E3', 'E7', 'E5'],
 ]
 
-from gomill_common import *
-
 def handicap_points(number_of_stones, board_size):
     """Return the handicap points for a given number of stones and board size.
 
@@ -26,12 +24,24 @@ def handicap_points(number_of_stones, board_size):
     number of handicap stones and board size.
 
     """
-    if board_size != 9:
+    if not 7 <= board_size <= 25:
         raise ValueError
     if number_of_stones <= 1:
         raise ValueError
-    try:
-        return [coords_from_vertex(s, board_size)
-                for s in handicap_9x9[number_of_stones-2]]
-    except IndexError:
-        raise ValueError
+    if board_size % 2 == 0 or board_size == 7:
+        if number_of_stones > 4:
+            raise ValueError
+    else:
+        if number_of_stones > 9:
+            raise ValueError
+    if board_size < 13:
+        altitude = 2
+    else:
+        altitude = 3
+    low = altitude
+    mid = (board_size - 1) / 2
+    high = board_size - altitude - 1
+    row_map = {'3' : low, '5' : mid, '7' : high}
+    col_map = {'C' : low, 'E' : mid, 'G' : high}
+    return [(row_map[s[1]], col_map[s[0]])
+            for s in handicap_pattern[number_of_stones-2]]
