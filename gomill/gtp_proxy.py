9a3ebba39fdc35d4f2efab5f21358fae22dacf95
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-07-17 12:46:34 +0100

    rework gtp_proxy API; support proxying handlers

--- gomill/gtp_proxy.py
+++ gomill/gtp_proxy.py
@@ -1,4 +1,9 @@
-"""FIXME"""
+"""Support for implementing proxy GTP engines.
+
+That is, engines which implement some or all of their commands by sending them
+on to another engine.
+
+"""
 
 from gomill import gtp_controller
 from gomill import gtp_engine
@@ -9,27 +14,69 @@ from gomill.gtp_engine import GtpError, GtpQuit, GtpFatalError
 class Gtp_proxy(object):
     """FIXME
 
-    """
-    def __init__(self):
-        self.controller = None
-        self.channel_id = None
-        self.client_commands = None
+    Instantiate with
+      channel_id -- string
+      controller -- Gtp_controller_protocol
+    These define the underlying engine.
 
-    def set_controller(self, controller, channel_id):
+    Public attributes:
+      channel_id -- string
+      controller -- Gtp_controller_protocol
+      engine     -- Gtp_engine_protocol
+
+    The 'engine' attribute is the proxy engine. Initially it supports all the
+    commands reported by 'list_commands' on the underlying engine. You can add
+    commands to it in the usual way; new commands will override any commands
+    with the same names in the underlying engine.
+
+    FIXME: Explain proxy.add_command.
+
+    Sample use:
+      channel = gtp_controller.Subprocess_gtp_channel([<command>])
+      controller = gtp_controller.Gtp_controller_protocol()
+      controller.add_channel('sub', channel)
+      proxy = gtp_proxy.Gtp_proxy('sub', controller)
+      proxy.add_command(...)
+      proxy.engine.add_command(...)
+      gtp_engine.run_interactive_gtp_session(proxy.engine)
+
+    """
+    def __init__(self, channel_id, controller):
+        self._set_controller(channel_id, controller)
+        self._make_engine()
+
+    def _set_controller(self, channel_id, controller):
         """FIXME
 
-        controller -- Gtp_controller_protocol
         channel_id -- string
+        controller -- Gtp_controller_protocol
+
+        This creates the engine, and sets the controller, channel_id, and engine
+        attributes.
 
         """
-        self.controller = controller
         self.channel_id = channel_id
+        self.controller = controller
         # FIXME: Be more lenient in what we accept? Ignore blank lines?
         client_commands = controller.do_command(channel_id, 'list_commands')\
                           .split("\n")
         self.client_commands = client_commands
 
+    def _make_engine(self):
+        """FIXME
+
+        returns a Gtp_engine_protocol
+
+        """
+        assert self.client_commands is not None
+        self.engine = gtp_engine.Gtp_engine_protocol()
+        self.engine.add_commands(self.get_handlers())
+        # FIXME: This overrides proxying for the protocol commands, but better
+        # not to make proxy handlers in the first place.
+        self.engine.add_protocol_commands()
+
     def pass_command(self, command, args):
+        """FIXME"""
         try:
             return self.controller.do_command(self.channel_id, command, *args)
         except GtpEngineError, e:
@@ -48,17 +95,22 @@ class Gtp_proxy(object):
             result[command] = handler
         return result
 
-    def make_engine(self):
-        """FIXME
+    def add_command(self, command, handler):
+        """Register a proxying handler function for a command.
 
-        returns a Gtp_engine_protocol
+        FIXME: Describe handler requirements: extra argument
 
         """
-        assert self.client_commands is not None
-        engine = gtp_engine.Gtp_engine_protocol()
-        engine.add_commands(self.get_handlers())
-        # FIXME: This overrides proxying for the protocol commands, but better
-        # not to make proxy handlers in the first place.
-        engine.add_protocol_commands()
-        return engine
+        def _handler(args):
+            return handler(self, args)
+        self.engine.add_command(command, _handler)
+
+    def add_commands(self, handlers):
+        """Register multiple proxying handler functions.
+
+        handlers -- dict command name -> handler
+
+        """
+        for command, handler in handlers.iteritems():
+            self.add_command(command, handler)
 
--- gomill_tests/test_gtp_proxy.py
+++ gomill_tests/test_gtp_proxy.py
@@ -2,22 +2,29 @@ from gomill import gtp_engine
 from gomill import gtp_controller
 from gomill import gtp_proxy
 
+def handle_mygenmove(proxy, args):
+    return proxy.pass_command('genmove', args)
+
+def handle_mytest(args):
+    return "mytest: %s" % ",".join(args)
+
 def main():
-    controller = gtp_controller.Gtp_controller_protocol()
     channel = gtp_controller.Subprocess_gtp_channel(
         "./player -m kiai.simple_montecarlo_player".split())
+    controller = gtp_controller.Gtp_controller_protocol()
     controller.add_channel("sub", channel)
 
-    proxy = gtp_proxy.Gtp_proxy()
-    proxy.set_controller(controller, 'sub')
-    engine = proxy.make_engine()
+    proxy = gtp_proxy.Gtp_proxy('sub', controller)
+    proxy.add_command('mygenmove', handle_mygenmove)
+    proxy.engine.add_command("mytest", handle_mytest)
 
-    is_error, response, end_session = engine.run_command("showboard", [])
+    is_error, response, end_session = proxy.engine.run_command("showboard", [])
     assert not is_error
     assert not end_session
     print response
 
-    gtp_engine.run_interactive_gtp_session(engine)
+    gtp_engine.run_interactive_gtp_session(proxy.engine)
+
 
 if __name__ == "__main__":
     main()
