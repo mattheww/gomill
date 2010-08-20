540abedf576867534db30b21cdc311e47d89cfe9
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-08-20 23:39:43 +0100

    Implement gomill_setup

---
+++ gomill_setup/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='gomill',
+      version="0.5",
+      description="Go programming toolkit",
+      author="Matthew Woodcraft",
+      author_email="matthew@woodcraft.me.uk",
+      packages=['gomill'],
+      scripts=['scripts/ringmaster'],
+      )
