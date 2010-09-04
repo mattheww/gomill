ac6c1f7abe9d9a8a1a88544224f43fd9da450736
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-04 16:35:53 +0100

    ringmaster_command_line: cleanups and fixes

--- gomill/ringmaster_command_line.py
+++ gomill/ringmaster_command_line.py
@@ -4,31 +4,25 @@ import os
 import sys
 from optparse import OptionParser
 
-from ringmaster import Ringmaster, RingmasterError
+from gomill import compact_tracebacks
+from gomill import job_manager # FIXME
+from gomill.ringmaster import Ringmaster, RingmasterError
 
-def do_run(ringmaster, worker_count=None, max_games=None):
+def do_run(ringmaster, options):
+    if options.log_gtp:
+        ringmaster.enable_gtp_logging()
+    if options.quiet:
+        ringmaster.set_quiet_mode()
     if ringmaster.status_file_exists():
         ringmaster.load_status()
     else:
         ringmaster.set_clean_status()
-    if worker_count is not None:
-        ringmaster.set_parallel_worker_count(worker_count)
-    ringmaster.run(max_games)
+    if options.parallel is not None:
+        ringmaster.set_parallel_worker_count(options.parallel)
+    ringmaster.run(options.max_games)
     ringmaster.report()
 
-def do_show(ringmaster):
-    if not ringmaster.status_file_exists():
-        raise RingmasterError("no status file")
-    ringmaster.load_status()
-    ringmaster.print_status_report()
-
-def do_report(ringmaster):
-    if not ringmaster.status_file_exists():
-        raise RingmasterError("no status file")
-    ringmaster.load_status()
-    ringmaster.report()
-
-def do_stop(ringmaster):
+def do_stop(ringmaster, options):
     try:
         f = open(ringmaster.command_pathname, "w")
         f.write("stop")
@@ -36,9 +30,38 @@ def do_stop(ringmaster):
     except EnvironmentError, e:
         raise RingmasterError("error writing command file:\n%s" % e)
 
-def do_reset(ringmaster):
+def do_show(ringmaster, options):
+    if not ringmaster.status_file_exists():
+        raise RingmasterError("no status file")
+    ringmaster.load_status()
+    ringmaster.print_status_report()
+
+def do_report(ringmaster, options):
+    if not ringmaster.status_file_exists():
+        raise RingmasterError("no status file")
+    ringmaster.load_status()
+    ringmaster.report()
+
+def do_reset(ringmaster, options):
     ringmaster.delete_state_and_output()
 
+def do_check(ringmaster, options):
+    if not ringmaster.check_players():
+        return 1
+
+def do_debugstatus(ringmaster, options):
+    ringmaster.print_status()
+
+
+_actions = {
+    "run" : do_run,
+    "stop" : do_stop,
+    "show" : do_show,
+    "report" : do_report,
+    "reset" : do_reset,
+    "check" : do_check,
+    "debugstatus" : do_debugstatus,
+    }
 
 def run(argv, ringmaster_class):
     usage = ("%prog [options] <control file> [command]\n\n"
@@ -61,38 +84,18 @@ def run(argv, ringmaster_class):
         command = "run"
     else:
         command = args[1]
-        if command not in ("run", "stop", "show", "report", "reset",
-                           "check", "debugstatus"):
-            parser.error("no such command: %s" % command)
+    try:
+        action = _actions[command]
+    except KeyError:
+        parser.error("no such command: %s" % command)
     tourn_pathname = args[0]
     if not tourn_pathname.endswith(".tourn"):
         parser.error("not a .tourn file")
-    exit_status = 0
     try:
         if not os.path.exists(tourn_pathname):
             raise RingmasterError("control file %s not found" % tourn_pathname)
         ringmaster = ringmaster_class(tourn_pathname)
-        if command == "run":
-            if options.log_gtp:
-                ringmaster.enable_gtp_logging()
-            if options.quiet:
-                ringmaster.set_quiet_mode()
-            do_run(ringmaster, options.parallel, options.max_games)
-        elif command == "show":
-            do_show(ringmaster)
-        elif command == "stop":
-            do_stop(ringmaster)
-        elif command == "report":
-            do_report(ringmaster)
-        elif command == "reset":
-            do_reset(ringmaster)
-        elif command == "check":
-            if not ringmaster.check_players():
-                exit_status = 1
-        elif command == "debugstatus":
-            ringmaster.print_status()
-        else:
-            raise AssertionError
+        exit_status = action(ringmaster, options)
     except RingmasterError, e:
         print >>sys.stderr, "ringmaster:", e
         exit_status = 1
