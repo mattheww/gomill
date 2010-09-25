c9c2b29e3e6fc947549277dac4317653e4ed2548
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-25 13:07:14 +0100

    boards_tests: test get(), play on occupied point

--- gomill_tests/board_tests.py
+++ gomill_tests/board_tests.py
@@ -1,3 +1,5 @@
+from __future__ import with_statement
+
 from gomill_tests import test_framework
 
 from gomill import boards
@@ -9,11 +11,16 @@ def test_basics(tc):
     b = boards.Board(9)
     tc.assertTrue(b.is_empty())
     tc.assertEqual(b.area_score(), 0)
+    tc.assertEqual(b.get(2, 3), None)
     b.play(2, 3, 'b')
+    tc.assertEqual(b.get(2, 3), 'b')
     tc.assertFalse(b.is_empty())
     tc.assertEqual(b.area_score(), 81)
     b.play(3, 4, 'w')
     tc.assertEqual(b.area_score(), 0)
+
+    with tc.assertRaises(ValueError):
+        b.play(3, 4, 'w')
 
     tc.assertItemsEqual(b.list_occupied_points(),
                         [('b', (2, 3)), ('w', (3, 4))])
