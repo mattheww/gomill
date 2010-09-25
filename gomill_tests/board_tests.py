ebc6af3bb55ce683199d7312e869009de6eaed4d
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-25 13:27:41 +0100

    More board tests: board_coords, copy()

--- gomill_tests/board_tests.py
+++ gomill_tests/board_tests.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 
+from gomill_tests import gomill_test_support
 from gomill_tests import test_framework
 
 from gomill import boards
@@ -7,9 +8,23 @@ from gomill import boards
 def make_tests(suite):
     suite.addTests(test_framework.make_simple_tests(globals()))
 
+def test_attributes(tc):
+    b = boards.Board(5)
+    tc.assertEqual(b.side, 5)
+    tc.assertEqual(
+        b.board_coords,
+        [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
+         (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
+         (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)])
+
 def test_basics(tc):
     b = boards.Board(9)
+
     tc.assertTrue(b.is_empty())
+    tc.assertItemsEqual(b.list_occupied_points(), [])
+
     tc.assertEqual(b.area_score(), 0)
     tc.assertEqual(b.get(2, 3), None)
     b.play(2, 3, 'b')
@@ -25,3 +40,14 @@ def test_basics(tc):
     tc.assertItemsEqual(b.list_occupied_points(),
                         [('b', (2, 3)), ('w', (3, 4))])
 
+
+def test_copy(tc):
+    b1 = boards.Board(9)
+    b1.play(2, 3, 'b')
+    b1.play(3, 4, 'w')
+    b2 = b1.copy()
+    gomill_test_support.check_boards_equal(b1, b2)
+    b2.play(5, 5, 'b')
+    b2.play(2, 1, 'b')
+    with tc.assertRaises(ValueError):
+        gomill_test_support.check_boards_equal(b1, b2)
---
+++ gomill_tests/gomill_test_support.py
@@ -0,0 +1,26 @@
+from gomill.gomill_common import *
+from gomill import ascii_boards
+
+def check_boards_equal(b1, b2):
+    """Check that two boards are equal.
+
+    Does nothing if they are equal; raises ValueError with a message if they
+    are not.
+
+    """
+    if b1.side != b2.side:
+        raise ValueError("size is different: %s, %s" % (b1.side, b2.side))
+    differences = []
+    for row, col in b1.board_coords:
+        if b1.get(row, col) != b2.get(row, col):
+            differences.append((row, col))
+    if not differences:
+        return
+    msg = "boards differ at %s" % " ".join(map(format_vertex, differences))
+    try:
+        msg += "\n%s\n%s" % (
+            ascii_boards.render_board(b1), ascii_boards.render_board(b2))
+    except Exception:
+        pass
+    raise ValueError(msg)
+
