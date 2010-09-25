5889e129d92e19990976f2ebdb8ead650a349865
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-25 11:14:27 +0100

    run_gomill_testsuite: do __import__ stuff so we can specify module names once

--- gomill_tests/run_gomill_testsuite.py
+++ gomill_tests/run_gomill_testsuite.py
@@ -1,10 +1,27 @@
+import sys
 import unittest2
 
-from gomill_tests import board_tests
+test_modules = [
+    'board_tests',
+    ]
+
+def get_test_modules():
+    """Import all _tests modules in the specified order.
+
+    Returns a list of module objects.
+
+    """
+    result = []
+    for name in test_modules:
+        dotted_name = "gomill_tests." + name
+        __import__(dotted_name)
+        result.append(sys.modules[dotted_name])
+    return result
 
 def main():
     suite = unittest2.TestSuite()
-    board_tests.make_tests(suite)
+    for mdl in get_test_modules():
+        mdl.make_tests(suite)
     runner = unittest2.TextTestRunner()
     runner.run(suite)
 
