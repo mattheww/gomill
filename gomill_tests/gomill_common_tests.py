f8ab1a7aa54b243b36ba6c53015722a6d778fca4
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-25 21:03:16 +0100

    test_coords_from_vertex

--- gomill_tests/gomill_common_tests.py
+++ gomill_tests/gomill_common_tests.py
@@ -30,6 +30,25 @@ def test_format_vertex_list(tc):
     tc.assertEqual(fvl([(0, 0), (1, 5)]), "A1,F2")
     tc.assertEqual(fvl([(0, 0), None, (1, 5)]), "A1,pass,F2")
 
+def test_coords_from_vertex(tc):
+    cv = gomill_common.coords_from_vertex
+    tc.assertEqual(cv("pass", 9), None)
+    tc.assertEqual(cv("pAss", 9), None)
+    tc.assertEqual(cv("A1", 9), (0, 0))
+    tc.assertEqual(cv("a1", 9), (0, 0))
+    tc.assertEqual(cv("A01", 9), (0, 0))
+    tc.assertEqual(cv("J9", 9), (8, 8))
+    tc.assertEqual(cv("M11", 19), (10, 11))
+    tc.assertRaises(ValueError, cv, "M11", 9)
+    tc.assertRaises(ValueError, cv, "K9", 9)
+    tc.assertRaises(ValueError, cv, "J10", 9)
+    tc.assertRaises(ValueError, cv, "I5", 9)
+    tc.assertRaises(ValueError, cv, "", 9)
+    tc.assertRaises(ValueError, cv, "29", 9)
+    tc.assertRaises(ValueError, cv, "@9", 9)
+    tc.assertRaises(ValueError, cv, "A-3", 9)
+    tc.assertRaises(ValueError, cv, None, 9)
+
 def test_sanitise_utf8(tc):
     su = gomill_common.sanitise_utf8
     tc.assertIsNone(su(None))
