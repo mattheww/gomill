c7615bb73ed711d14810fc03821baa89a7b4d037
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-26 13:23:52 +0100

    test_linebased_channel_responses, test_linebased_channel_response_cleaning

--- gomill_tests/gtp_controller_tests.py
+++ gomill_tests/gtp_controller_tests.py
@@ -26,6 +26,43 @@ def test_linebased_channel(tc):
         channel.get_response)
     channel.close()
 
+def test_linebased_channel_responses(tc):
+    channel = Preprogrammed_gtp_channel("= 2\n\n? unknown command\n\n")
+    channel.send_command("protocol_version", [])
+    tc.assertEqual(channel.get_response(), (False, "2"))
+    channel.send_command("xyzzy", ["1", "2"])
+    tc.assertEqual(channel.get_response(), (True, "unknown command"))
+
+def test_linebased_channel_response_cleaning(tc):
+    channel = Preprogrammed_gtp_channel(
+        # ignores CRs (GTP spec)
+        "= 1abc\rde\r\n\n"
+        # FIXME: really ignores CRs (GTP spec) [broken?]
+        #"= 1abc\der\n\r\n"
+        # ignores extra blank lines (GTP spec)
+        "= 2abcde\n\n\n\n"
+        # strips control characters (GTP spec)
+        "= 3a\x7fbc\x00d\x07e\n\n"
+        # converts tabs to spaces (GTP spec)
+        "= 4abc\tde\n\n"
+        # strips leading whitespace (channel docs)
+        "=  \t   5abcde\n\n"
+        # strips trailing whitepace (channel docs)
+        "= 6abcde  \t  \n\n"
+        # doesn't strip whitespace in the middle of a multiline response
+        "= 7aaa  \n  bbb\tccc\nddd  \t  \n\n"
+        # all this at once, in a failure response
+        "?    a\raa  \r\n  b\rbb\tcc\x01c\nddd  \t  \n\n"
+        )
+    tc.assertEqual(channel.get_response(), (False, "1abcde"))
+    tc.assertEqual(channel.get_response(), (False, "2abcde"))
+    tc.assertEqual(channel.get_response(), (False, "3abcde"))
+    tc.assertEqual(channel.get_response(), (False, "4abc de"))
+    tc.assertEqual(channel.get_response(), (False, "5abcde"))
+    tc.assertEqual(channel.get_response(), (False, "6abcde"))
+    tc.assertEqual(channel.get_response(), (False, "7aaa  \n  bbb ccc\nddd"))
+    tc.assertEqual(channel.get_response(), (True, "aaa  \n  bbb ccc\nddd"))
+
 def test_linebased_channel_without_response(tc):
     channel = Preprogrammed_gtp_channel("")
     channel.send_command("protocol_version", [])
