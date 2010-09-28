9d572cc83fe56752399ea403107a9abfbe7b9d56
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-28 08:47:27 +0100

    Simple test_proxy()

--- gomill_tests/gtp_proxy_tests.py
+++ gomill_tests/gtp_proxy_tests.py
@@ -9,3 +9,12 @@ from gomill import gtp_proxy
 def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
 
+
+def test_proxy(tc):
+    channel = gtp_controller_test_support.get_test_channel()
+    controller = gtp_controller.Gtp_controller(channel, 'testbackend')
+    proxy = gtp_proxy.Gtp_proxy()
+    proxy.set_back_end_controller(controller)
+    tc.assertEqual(
+        proxy.engine.run_command('test', ['ab', 'cd']),
+        (False, 'args: ab cd', False))
