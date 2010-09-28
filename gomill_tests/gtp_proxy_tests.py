2ac31d166554c8e31410e1cf0dd76cb278e8f4d0
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-09-28 19:04:07 +0100

    gtp_proxy_tests: test_passthrough

--- gomill_tests/gtp_proxy_tests.py
+++ gomill_tests/gtp_proxy_tests.py
@@ -9,12 +9,35 @@ from gomill import gtp_proxy
 def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
 
-
-def test_proxy(tc):
+def _make_proxy():
     channel = gtp_controller_test_support.get_test_channel()
     controller = gtp_controller.Gtp_controller(channel, 'testbackend')
     proxy = gtp_proxy.Gtp_proxy()
     proxy.set_back_end_controller(controller)
+    return proxy
+
+def test_proxy(tc):
+    proxy = _make_proxy()
     tc.assertEqual(
         proxy.engine.run_command('test', ['ab', 'cd']),
         (False, 'args: ab cd', False))
+    proxy.close()
+
+def test_passthrough(tc):
+    proxy = _make_proxy()
+    tc.assertEqual(
+        proxy.engine.run_command(
+            'known_command', ['gomill-passthrough']),
+        (False, 'true', False))
+    tc.assertEqual(
+        proxy.engine.run_command(
+            'test', ['ab', 'cd']),
+        (False, 'args: ab cd', False))
+    tc.assertEqual(
+        proxy.engine.run_command(
+            'gomill-passthrough', ['known_command', 'gomill-passthrough']),
+        (False, 'false', False))
+    tc.assertEqual(
+        proxy.engine.run_command('gomill-passthrough', []),
+        (True, 'invalid arguments', False))
+
