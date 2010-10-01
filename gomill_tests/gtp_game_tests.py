792af71f0abc086f72181ba19bd981c98cc232b2
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-01 22:29:39 +0100

    check game and result attributes

--- gomill_tests/gtp_game_tests.py
+++ gomill_tests/gtp_game_tests.py
@@ -35,9 +35,28 @@ class Game_fixture(test_framework.Fixture):
 
 def test_game(tc):
     fx = Game_fixture(tc)
+    tc.assertDictEqual(fx.game.players, {'b' : 'one', 'w' : 'two'})
     fx.game.use_internal_scorer()
     fx.game.ready()
+    tc.assertIsNone(fx.game.result)
     fx.game.run()
     fx.game.close_players()
     tc.assertIsNone(fx.game.describe_late_errors())
+    tc.assertEqual(fx.game.result.player_b, 'one')
+    tc.assertEqual(fx.game.result.player_w, 'two')
+    tc.assertEqual(fx.game.result.winning_colour, 'b')
+    tc.assertEqual(fx.game.result.winning_player, 'one')
     tc.assertEqual(fx.game.result.sgf_result, "B+18")
+    tc.assertEqual(fx.game.result.detail, None)
+    tc.assertDictEqual(fx.game.result.cpu_times, {'one' : None, 'two' : None})
+    tc.assertListEqual(fx.game.moves, [
+        ('b', (0, 4), None), ('w', (0, 6), None),
+        ('b', (1, 4), None), ('w', (1, 6), None),
+        ('b', (2, 4), None), ('w', (2, 6), None),
+        ('b', (3, 4), None), ('w', (3, 6), None),
+        ('b', (4, 4), None), ('w', (4, 6), None),
+        ('b', (5, 4), None), ('w', (5, 6), None),
+        ('b', (6, 4), None), ('w', (6, 6), None),
+        ('b', (7, 4), None), ('w', (7, 6), None),
+        ('b', (8, 4), None), ('w', (8, 6), None),
+        ('b', None, None), ('w', None, None)])
