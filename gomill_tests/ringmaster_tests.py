3f452c05470678e5590680d1c925407e59c84667
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-02 22:39:34 +0100

    ringmaster_tests: use a string.Template for the control file

--- gomill_tests/ringmaster_tests.py
+++ gomill_tests/ringmaster_tests.py
@@ -1,6 +1,7 @@
 """Tests for ringmaster.py."""
 
 import os
+import string
 
 from gomill_tests import test_framework
 from gomill_tests import gomill_test_support
@@ -9,57 +10,6 @@ from gomill_tests import gtp_engine_fixtures
 
 def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
-
-test1_ctl = """
-
-competition_type = 'playoff'
-
-description = "gomill_tests playoff 'test1'."
-
-players = {
-    'gtptest'  : Player("test", stderr=DISCARD),
-    }
-
-move_limit = 400
-record_games = False
-board_size = 9
-komi = 7.5
-scorer = "internal"
-
-number_of_games = 400
-
-matchups = [
-    Matchup('gtptest', 'gtptest'),
-    ]
-
-"""
-
-test2_ctl = """
-
-competition_type = 'playoff'
-
-description = "gomill_tests playoff 'test1'."
-
-players = {
-    'gtptest'  : Player("test", stderr=DISCARD),
-    'failer'   : Player("test fail=startup", stderr=DISCARD),
-    }
-
-move_limit = 400
-record_games = False
-board_size = 9
-komi = 7.5
-scorer = "internal"
-
-number_of_games = 400
-
-matchups = [
-    Matchup('gtptest', 'failer', handicap=6, handicap_style='fixed'),
-    ]
-
-"""
-
-
 
 class Ringmaster_fixture(test_framework.Fixture):
     """Fixture setting up a Ringmaster with mock suprocesses.
@@ -75,12 +25,45 @@ class Ringmaster_fixture(test_framework.Fixture):
         self.ringmaster.set_display_mode('test')
         self.msf = gtp_engine_fixtures.Mock_subprocess_fixture(tc)
 
+simple_ctl = string.Template("""
+
+competition_type = 'playoff'
+
+description = 'gomill_tests playoff.'
+
+players = {
+    'p1'  : Player("test ${cmdline1}", stderr=DISCARD),
+    'p2'  : Player("test ${cmdline2}", stderr=DISCARD),
+    }
+
+move_limit = 400
+record_games = False
+board_size = 9
+komi = 7.5
+scorer = 'internal'
+
+number_of_games = 400
+
+matchups = [
+    Matchup('p1', 'p2'),
+    ]
+
+""")
+
 def test_check_players(tc):
-    fx = Ringmaster_fixture(tc, test1_ctl)
+    vals = {
+        'cmdline1' : "",
+        'cmdline2' : "",
+        }
+    fx = Ringmaster_fixture(tc, simple_ctl.substitute(vals))
     tc.assertTrue(fx.ringmaster.check_players(discard_stderr=True))
 
 def test_run(tc):
-    fx = Ringmaster_fixture(tc, test1_ctl)
+    vals = {
+        'cmdline1' : "",
+        'cmdline2' : "",
+        }
+    fx = Ringmaster_fixture(tc, simple_ctl.substitute(vals))
     fx.ringmaster.set_clean_status()
     fx.ringmaster.run(max_games=3)
     tc.assertListEqual(
@@ -88,17 +71,25 @@ def test_run(tc):
         [])
 
 def test_check_players_fail(tc):
-    fx = Ringmaster_fixture(tc, test2_ctl)
+    vals = {
+        'cmdline1' : "",
+        'cmdline2' : "fail=startup",
+        }
+    fx = Ringmaster_fixture(tc, simple_ctl.substitute(vals))
     tc.assertFalse(fx.ringmaster.check_players(discard_stderr=True))
 
 def test_run_fail(tc):
-    fx = Ringmaster_fixture(tc, test2_ctl)
+    vals = {
+        'cmdline1' : "",
+        'cmdline2' : "fail=startup",
+        }
+    fx = Ringmaster_fixture(tc, simple_ctl.substitute(vals))
     fx.ringmaster.set_clean_status()
     fx.ringmaster.run()
     tc.assertListEqual(
         fx.ringmaster.presenter.retrieve_warnings(),
         ["game 0_0 -- aborting game due to error:\n"
-         "error starting subprocess for player failer:\n"
+         "error starting subprocess for player p2:\n"
          "exec forced to fail",
          "halting run due to void games"])
 
