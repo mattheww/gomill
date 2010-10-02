c897efa7c2c85bb37e28a8f5c9ffdf5218c28f2d
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-02 11:36:23 +0100

    Player_check_fixture

--- gomill_tests/game_job_tests.py
+++ gomill_tests/game_job_tests.py
@@ -3,6 +3,7 @@
 from gomill import gtp_controller
 from gomill import game_jobs
 
+from gomill_tests import test_framework
 from gomill_tests import gomill_test_support
 from gomill_tests import gtp_engine_fixtures
 
@@ -10,55 +11,48 @@ def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
 
 
+class Player_check_fixture(test_framework.Fixture):
+    """Fixture setting up a Player_check.
+
+    attributes:
+      player -- game_jobs.Player
+      check  -- game_jobs.Player_check
+
+    """
+    def __init__(self, tc):
+        self.player = game_jobs.Player()
+        self.player.code = 'test'
+        self.player.cmd_args = ['test']
+        self.check = game_jobs.Player_check()
+        self.check.player = self.player
+        self.check.board_size = 9
+        self.check.komi = 7.0
+
 def test_check_player(tc):
     fx = gtp_engine_fixtures.Mock_subprocess_fixture(tc)
-
-    player = game_jobs.Player()
-    player.code = 'test'
-    player.cmd_args = ['test']
-
-    check = game_jobs.Player_check()
-    check.player = player
-    check.board_size = 9
-    check.komi = 7.0
-
-    game_jobs.check_player(check)
+    ck = Player_check_fixture(tc)
+    game_jobs.check_player(ck.check)
 
 def test_check_player_boardsize_fails(tc):
     fx = gtp_engine_fixtures.Mock_subprocess_fixture(tc)
     engine = gtp_engine_fixtures.get_test_engine()
     fx.register_engine('no_boardsize', engine)
-
-    player = game_jobs.Player()
-    player.code = 'test'
-    player.cmd_args = ['no_boardsize']
-
-    check = game_jobs.Player_check()
-    check.player = player
-    check.board_size = 9
-    check.komi = 7.0
+    ck = Player_check_fixture(tc)
+    ck.player.cmd_args = ['no_boardsize']
 
     with tc.assertRaises(game_jobs.CheckFailed) as ar:
-        game_jobs.check_player(check)
+        game_jobs.check_player(ck.check)
     tc.assertEqual(str(ar.exception),
                    "failure response from 'boardsize 9' to test:\n"
                    "unknown command")
 
 def test_check_player_startup_gtp_commands(tc):
     fx = gtp_engine_fixtures.Mock_subprocess_fixture(tc)
-
-    player = game_jobs.Player()
-    player.code = 'test'
-    player.cmd_args = ['test']
-    player.startup_gtp_commands = [('list_commands', []),
-                                   ('nonexistent', ['command'])]
-    check = game_jobs.Player_check()
-    check.player = player
-    check.board_size = 9
-    check.komi = 7.0
-
+    ck = Player_check_fixture(tc)
+    ck.player.startup_gtp_commands = [('list_commands', []),
+                                       ('nonexistent', ['command'])]
     with tc.assertRaises(game_jobs.CheckFailed) as ar:
-        game_jobs.check_player(check)
+        game_jobs.check_player(ck.check)
     tc.assertEqual(str(ar.exception),
                    "failure response from 'nonexistent command' to test:\n"
                    "unknown command")
