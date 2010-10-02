098db05ece3cd2b2a9af2a54f12edc88e7168cc4
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-02 16:29:59 +0100

    test_run_gtp_session

--- gomill_tests/gtp_engine_tests.py
+++ gomill_tests/gtp_engine_tests.py
@@ -4,6 +4,7 @@ from gomill import gtp_engine
 
 from gomill_tests import gomill_test_support
 from gomill_tests import gtp_engine_test_support
+from gomill_tests import gtp_controller_test_support
 
 def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
@@ -21,4 +22,17 @@ def test_engine(tc):
 
     check_engine = gtp_engine_test_support.check_engine
     check_engine(tc, engine, 'test', ['ab', 'cd'], "args: ab cd")
+
+def test_run_gtp_session(tc):
+    engine = gtp_engine.Gtp_engine_protocol()
+    engine.add_protocol_commands()
+
+    stream = "known_command list_commands\nxyzzy\nquit\n"
+    command_pipe = gtp_controller_test_support.Mock_reading_pipe(stream)
+    response_pipe = gtp_controller_test_support.Mock_writing_pipe()
+    gtp_engine.run_gtp_session(engine, command_pipe, response_pipe)
+    tc.assertMultiLineEqual(response_pipe.getvalue(),
+                            "= true\n\n? unknown command\n\n=\n\n")
+    command_pipe.close()
+    response_pipe.close()
 
