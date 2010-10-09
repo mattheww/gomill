456aaadd9ba6eedf65a54b9e9e12efb5eb850453
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-09 21:08:59 +0100

    mcts_tuner_tests: exercise the user-defined functions

--- gomill_tests/mcts_tuner_tests.py
+++ gomill_tests/mcts_tuner_tests.py
@@ -1,5 +1,7 @@
 """Tests for mcts_tuners.py"""
 
+from textwrap import dedent
+
 from gomill import mcts_tuners
 from gomill.mcts_tuners import Parameter_config
 from gomill.competitions import (
@@ -13,6 +15,9 @@ def make_tests(suite):
 
 def trivial_scale_fn(f):
     return f
+
+def times_100_fn(f):
+    return int(100*f)
 
 def simple_make_candidate(*args):
     return Player_config("cand " + " ".join(map(str, args)))
@@ -38,7 +43,7 @@ def test_parameter_config(tc):
 
             Parameter_config(
                 'initial_wins',
-                scale_fn = trivial_scale_fn,
+                scale_fn = times_100_fn,
                 format = "iwins %d"),
             ],
         'make_candidate' : simple_make_candidate,
@@ -47,3 +52,19 @@ def test_parameter_config(tc):
         'subdivisions' : 2,
         }
     comp.initialise_from_control_file(config)
+    tc.assertEqual(comp.format_parameters((0.5, 23)), "rsn@ 0.50; iwins 23")
+    tc.assertEqual(comp.format_parameters(('x', 3)), "[resign_at?x]; iwins 3")
+    tc.assertEqual(comp.scale_parameters((0.5, 0.23)), (0.5, 23))
+    with tc.assertRaises(CompetitionError) as ar:
+        comp.scale_parameters((0.5, None))
+    tc.assertTracebackStringEqual(str(ar.exception), dedent("""\
+    error from scale_fn for initial_wins
+    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+    traceback (most recent call last):
+    mcts_tuner_tests|times_100_fn
+    failing line:
+    return int(100*f)
+    """))
+    cand = comp.make_candidate('c#1', (0.5, 0.23))
+    tc.assertEqual(cand.code, 'c#1')
+    tc.assertListEqual(cand.cmd_args, ['cand', '0.5', '23'])
