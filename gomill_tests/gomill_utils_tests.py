9a84f58c211f020cc30424f112a6711bd52edd77
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-23 11:48:47 +0100

    test_format_float()

--- gomill_tests/gomill_utils_tests.py
+++ gomill_tests/gomill_utils_tests.py
@@ -8,6 +8,12 @@ def make_tests(suite):
     suite.addTests(gomill_test_support.make_simple_tests(globals()))
 
 
+def test_format_float(tc):
+    ff = gomill_utils.format_float
+    tc.assertEqual(ff(1), "1")
+    tc.assertEqual(ff(1.0), "1")
+    tc.assertEqual(ff(1.5), "1.5")
+
 def test_sanitise_utf8(tc):
     su = gomill_utils.sanitise_utf8
     tc.assertIsNone(su(None))
