bbf77eb2989263e6ed04c001367eabe0110902a9
Matthew Woodcraft <matthew@woodcraft.me.uk>
2010-10-24 21:40:11 +0100

    gomill setup.py: metadata

--- gomill_setup/setup.py
+++ gomill_setup/setup.py
@@ -10,12 +10,39 @@ if BuildDoc:
 else:
     cmdclass = {}
 
+long_description = """\
+Gomill is a suite of tools, and a Python library, for use in developing and
+testing Go-playing programs. It is based around the Go Text Protocol (GTP) and
+the Smart Game Format (SGF).
+
+The principal tool is the ringmaster, which plays programs against each other
+and keeps track of the results.
+"""
+
 setup(name='gomill',
       version="0.5",
       description="Go programming toolkit",
+      long_description=long_description,
       author="Matthew Woodcraft",
       author_email="matthew@woodcraft.me.uk",
       packages=['gomill'],
       scripts=['ringmaster'],
       cmdclass=cmdclass,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: POSIX",
+          "Operating System :: MacOS :: MacOS X",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.5",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python",
+          "Topic :: Games/Entertainment :: Board Games",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      keywords="go baduk weiqi",
       )
