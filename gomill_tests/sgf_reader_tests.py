4d9b2048b3700fc0c802c07230eb7eabe1e9d398
Matthew Woodcraft <matthew@woodcraft.me.uk>
2011-06-25 23:05:58 +0100

    sgf_reader_tests: one more malformed case

--- gomill_tests/sgf_reader_tests.py
+++ gomill_tests/sgf_reader_tests.py
@@ -54,6 +54,7 @@ def test_malformed(tc):
     tc.assertRaises(ValueError, read, r"(;+B[ag])")
     tc.assertRaises(ValueError, read, r"(;B+[ag])")
     tc.assertRaises(ValueError, read, r"(;[B][ag])")
+    tc.assertRaises(ValueError, read, r"(;B W[ag])")
 
     # We don't reject these yet, because we don't track parens
     #tc.assertRaises(ValueError, read, r"(;B[ag];W[ah](;B[ai])")
