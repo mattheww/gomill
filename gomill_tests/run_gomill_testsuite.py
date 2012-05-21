dca1d53b141e409211cabdc05183f0ff2f577a31
Matthew Woodcraft <matthew@woodcraft.me.uk>
2012-05-21 22:21:21 +0100

    run_gomill_testsuite: support running individual tests

--- gomill_tests/run_gomill_testsuite.py
+++ gomill_tests/run_gomill_testsuite.py
@@ -1,5 +1,6 @@
 """Construct and run the gomill testsuite."""
 
+from collections import defaultdict
 import sys
 from optparse import OptionParser
 
@@ -34,20 +35,12 @@ def get_test_module(name):
     __import__(dotted_name)
     return sys.modules[dotted_name]
 
-def get_test_modules():
-    """Import all _tests modules in the specified order.
+def run_testsuite(suite, failfast, buffer):
+    """Run the specified testsuite.
 
-    Returns a list of module objects.
-
-    """
-    return [get_test_module(name) for name in test_modules]
-
-def run_testsuite(module_names, failfast, buffer):
-    """Run the gomill testsuite.
-
-    module_names -- names of modules from gomill_tests, or None for all
-    failfast     -- bool (stop at first failing test)
-    buffer       -- bool (show stderr/stdout only for failing tests)
+    suite    -- TestSuite
+    failfast -- bool (stop at first failing test)
+    buffer   -- bool (show stderr/stdout only for failing tests)
 
     Output is to stderr
 
@@ -57,31 +50,81 @@ def run_testsuite(module_names, failfast, buffer):
         unittest2.signals.installHandler()
     except Exception:
         pass
-    if module_names is None:
-        modules = get_test_modules()
-    else:
-        modules = [get_test_module(name) for name in module_names]
-    suite = unittest2.TestSuite()
-    for mdl in modules:
-        mdl.make_tests(suite)
     runner = unittest2.TextTestRunner(failfast=failfast, buffer=buffer)
     runner.run(suite)
 
+class UnknownTest(StandardError):
+    """Unknown test module or test name."""
+
+def make_testsuite(module_names, tests_by_module):
+    """Import testsuite modules and make the TestCases.
+
+    module_names    -- set of module names (empty means all)
+    tests_by_module -- map module_name -> set of test names (empty means all)
+
+    Returns a TestSuite.
+
+    Test names are as given by test.id()
+    For function-based tests, that means <module_name>.<function_name>
+    For parameterised tests, it's <module_name>.<test_name>:<code>
+
+    The tests in the returned suite are always in their 'natural' order; the
+    order of command line items has no effect.
+
+    Raises UnknownTest if a specified test or test module doesn't exist.
+
+    """
+    result = unittest2.TestSuite()
+    for module_name in sorted(module_names):
+        if module_name not in test_modules:
+            raise UnknownTest("unknown module: %s" % module_name)
+    for module_name in test_modules:
+        if module_names and (module_name not in module_names):
+            continue
+        mdl = get_test_module(module_name)
+        suite = unittest2.TestSuite()
+        mdl.make_tests(suite)
+        test_names = tests_by_module[module_name]
+        if not test_names:
+            result.addTests(suite)
+            continue
+        existing = set(test.id() for test in suite)
+        for test_name in sorted(test_names):
+            if test_name not in existing:
+                raise UnknownTest("unknown test: %s" % test_name)
+        for test in suite:
+            if test.id() in test_names:
+                result.addTest(test)
+    return result
+
+def interpret_args(args):
+    """Interpret command-line arguments.
+
+    Returns a pair (module_names, tests_by_module), for make_testsuite().
+
+    """
+    tests_by_module = defaultdict(set)
+    module_names = set()
+    for arg in args:
+        module_name, is_compound, _ = arg.partition(".")
+        module_names.add(module_name)
+        if is_compound:
+            tests_by_module[module_name].add(arg)
+    return module_names, tests_by_module
+
 def run(argv):
-    parser = OptionParser(usage="%prog [options] [module] ...")
+    parser = OptionParser(usage="%prog [options] [module|module.test] ...")
     parser.add_option("-f", "--failfast", action="store_true",
                       help="stop after first test")
     parser.add_option("-p", "--nobuffer", action="store_true",
                       help="show stderr/stdout for successful tests")
     (options, args) = parser.parse_args(argv)
-    if args:
-        module_names = args
-        for module_name in module_names:
-            if module_name not in test_modules:
-                parser.error("unknown module: %s" % module_name)
-    else:
-        module_names = None
-    run_testsuite(module_names, options.failfast, not options.nobuffer)
+    module_names, tests_by_module = interpret_args(args)
+    try:
+        suite = make_testsuite(module_names, tests_by_module)
+    except UnknownTest, e:
+        parser.error(str(e))
+    run_testsuite(suite, options.failfast, not options.nobuffer)
 
 def import_unittest():
     """Import unittest2 into global scope.
