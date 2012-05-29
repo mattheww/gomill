7dbfd396e72d96b89b4c64cad3a98482e7586734
Matthew Woodcraft <matthew@woodcraft.me.uk>
2012-05-29 21:09:14 +0100

    test_twogtp.py: test SGF output

--- gomill_process_tests/test_twogtp.py
+++ gomill_process_tests/test_twogtp.py
@@ -9,17 +9,34 @@ Test output may need adjusting if gnugo RNG changes.
 
 """
 
+import os
+import re
+import shutil
 import subprocess
+import tempfile
+from operator import itemgetter
+
+from gomill import __version__
+
+class Test(object):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('sgf', None)
+        kwargs.setdefault('exit_status', 0)
+        self.__dict__.update(kwargs)
+
 
 tests = [
 
-("failgenmove", 0, """
+Test(
+code="failgenmove",
+command="""
 gomill_examples/twogtp
 --black='gnugo --mode=gtp --level=0 --seed=1'
 --white='gomill_process_tests/gtp_test_player --fail-command=genmove'
 --size=9
 --verbose=1
-""", """\
+""",
+output="""\
 Black: GNU Go:3.8
 White: GTP test player
 B E5
@@ -27,46 +44,59 @@ gnugo beat gomill_process_tests/gtp_test_player B+F (forfeit by gomill_process_t
 forced to fail from command line)
 """),
 
-("failgtp", 1, """
+Test(
+code="failgtp",
+command="""
 gomill_examples/twogtp
 --black='echo fail'
 --white='gomill_process_tests/gtp_test_player --fail-command=genmove'
 --size=9
 --verbose=1
-""", """\
+""",
+output="""\
 error creating players:
 GTP protocol error reading response to first command (protocol_version) from player echo:
 engine isn't speaking GTP: first byte is 'f'
 
-"""),
+""",
+exit_status=1),
 
-("samename", 0, """
+Test(
+code="samename",
+command="""
 gomill_examples/twogtp
 --black='gomill_examples/gtp_test_player'
 --white='gomill_examples/gtp_test_player'
 --size=9
-""", """\
+""",
+output="""\
 gomill_examples/gtp_test_player-b vs gomill_examples/gtp_test_player-w ? (no score reported)
 """),
 
-("gnugo", 0, """
+Test(
+code="gnugo",
+command="""
 gomill_examples/twogtp
 --black='gnugo --mode=gtp --level=0 --seed=1'
 --white='/usr/games/gnugo --mode=gtp --level=0 --seed=2'
 --size=5
 --games=2
-""", """\
+""",
+output="""\
 gnugo beat /usr/games/gnugo B+R
 gnugo beat /usr/games/gnugo B+R
 """),
 
-("verbose-1", 0, """
+Test(
+code="verbose-1",
+command="""
 gomill_examples/twogtp
 --black='gnugo --mode=gtp --level=0 --seed=1'
 --white='/usr/games/gnugo --mode=gtp --level=0 --seed=2'
 --size=5
 --verbose=1
-""", """\
+""",
+output="""\
 Black: GNU Go:3.8
 White: GNU Go:3.8
 B C3
@@ -87,13 +117,16 @@ B C4
 gnugo beat /usr/games/gnugo B+R
 """),
 
-("verbose-2", 0, """
+Test(
+code="verbose-2",
+command="""
 gomill_examples/twogtp
 --black='gnugo --mode=gtp --level=0 --seed=1'
 --white='/usr/games/gnugo --mode=gtp --level=0 --seed=2'
 --size=3
 --verbose=2
-""", """\
+""",
+output="""\
 Black: GNU Go:3.8
 
 White: GNU Go:3.8
@@ -107,13 +140,42 @@ B B2
 gnugo beat /usr/games/gnugo B+R
 """),
 
+Test(
+code="sgf",
+command="""
+gomill_examples/twogtp
+--black='gnugo --mode=gtp --level=0 --seed=1'
+--white='/usr/games/gnugo --mode=gtp --level=0 --seed=2'
+--size=3 --sgfbase=%(sgfbase)s
+""",
+output="""\
+gnugo beat /usr/games/gnugo B+R
+""",
+sgf="(;FF[4]AP[Gomill twogtp:VER]CA[UTF-8]DT[***]GM[1]KM[7.5]PB[GNU Go:3.8]"
+    "PW[GNU Go:3.8]RE[B+R]SZ[3];B[bb]C[gnugo beat /usr/games/gnugo B+R])"
+),
+
 ]
 
-def testrun(code, expected_status, cmd, expected_output):
-    print "** %s" % code
+def scrub_sgf(s):
+    """Normalise sgf string for convenience of testing.
+
+    Replaces dates with '***', and 'twogtp:<__version__>' with 'twogtp:VER'.
+
+    Removes newlines.
+
+    """
+    s = re.sub(r"(?<=DT\[)[-0-9]+(?=\])", "***", s)
+    s = re.sub(r"twogtp:" + re.escape(__version__), "twogtp:VER", s)
+    s = s.replace("\n", "")
+    return s
+
+def testrun(test, sandbox_dir):
+    print "** %s" % test.code
+    sgfbase = os.path.join(sandbox_dir, test.code)
     try:
         output = subprocess.check_output(
-            cmd.replace("\n", " "),
+            test.command.replace("\n", " ") % locals(),
             shell=True,
             stderr=subprocess.STDOUT)
         status = 0
@@ -121,14 +183,20 @@ def testrun(code, expected_status, cmd, expected_output):
         status = e.returncode
         output = e.output
     passed = True
-    if output != expected_output:
+    if output != test.output:
         passed = False
         print "BAD OUTPUT"
         print output
-    if status != expected_status:
+    if status != test.exit_status:
         passed = False
         print "BAD EXIT STATUS"
         print status
+    if test.sgf:
+        sgf_written = open("%s/%s000.sgf" % (sandbox_dir, test.code)).read()
+        if scrub_sgf(sgf_written) != test.sgf:
+            passed = False
+            print "BAD SGF"
+            print sgf_written
     if passed:
         print "TEST PASSED"
     else:
@@ -138,11 +206,15 @@ def testrun(code, expected_status, cmd, expected_output):
 SKIP = set()
 
 def main():
-    for t in tests:
-        if t[0] in SKIP:
-            print "## %s: SKIP" % t[0]
-            continue
-        testrun(*t)
+    dirname = tempfile.mkdtemp(prefix='test_twogtp')
+    try:
+        for test in tests:
+            if test.code in SKIP:
+                print "## %s: SKIP" % test.code
+                continue
+            testrun(test, dirname)
+    finally:
+        shutil.rmtree(dirname)
 
 if __name__ == "__main__":
     main()
