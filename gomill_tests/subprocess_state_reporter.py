5cc5163e27a220623cd1410bb830cdee0de60c45
Matthew Woodcraft <matthew@woodcraft.me.uk>
2012-06-05 17:55:52 +0100

    remove subprocess_state_reporter diagnostics

--- gomill_tests/subprocess_state_reporter.py
+++ gomill_tests/subprocess_state_reporter.py
@@ -8,26 +8,15 @@ This mustn't import any gomill or gomill_tests code.
 import sys
 import os
 
-LOG_FILE = "/home/mjw/kiai/tmp/ssr.log"
-
 def main():
-    logfile = open(LOG_FILE, "w")
-    def log(s):
-        logfile.write(s)
-        logfile.flush()
-    try:
-        sys.stderr.write("subprocess_state_reporter: testing\n")
-        # Read the GTP command
-        sys.stdin.readline()
-        if "--extra-stderr" in sys.argv:
-            for i in xrange(500):
-                sys.stderr.write("blah\n")
-        sys.stdout.write("= cwd: %s\nGOMILL_TEST:%s\n\n" %
-                         (os.getcwd(), os.environ.get("GOMILL_TEST")))
-    except Exception, e:
-        #log(str(e))
-        sys.stderr = logfile
-        raise
+    sys.stderr.write("subprocess_state_reporter: testing\n")
+    # Read the GTP command
+    sys.stdin.readline()
+    if "--extra-stderr" in sys.argv:
+        for i in xrange(500):
+            sys.stderr.write("blah\n")
+    sys.stdout.write("= cwd: %s\nGOMILL_TEST:%s\n\n" %
+                     (os.getcwd(), os.environ.get("GOMILL_TEST")))
 
 if __name__ == "__main__":
     main()
